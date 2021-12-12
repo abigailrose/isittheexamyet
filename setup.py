3a690269a05aaa9241ae744bce3731140bb0ec68
Abigail Fox <abigailfireflyfox@gmail.com>
2021-12-12 17:35:35 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+Adapted from Insta485 python package configuration.
+
+Andrew DeOrio <awdeorio@umich.edu>
+"""
+
+from setuptools import setup
+
+setup(
+    name='exam',
+    version='0.1.0',
+    packages=['exam'],
+    include_package_data=True,
+    install_requires=[
+        'arrow',
+        'bs4',
+        'Flask',
+        'html5validator',
+        'pycodestyle',
+        'pydocstyle',
+        'pylint',
+        'pytest',
+        'pytest-mock',
+        'requests',
+    ],
+    python_requires='>=3.6',
+)
