9126a57b623af59cf2c9e97a549428e716e20b91
Abigail Fox <abigailfireflyfox@gmail.com>
2021-12-12 18:21:00 -0500

    configured datetime

--- exam/exam.py
+++ exam/exam.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    header = "<h1>Has the EECS485 exam started yet?</h1>"
+    answer = ""
+
+    current_time = datetime.datetime.now()
+    if current_time.month == 12 and current_time.day == 14:
+        answer = "<p>yes</p>"
+    else:
+        answer = "<p>no</p>"
+
+    response = header + answer
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
